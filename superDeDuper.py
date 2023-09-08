e415a43f604949eff8e94b7968b07e8d338c8b7a
James Wesley Goedert <jwgoedert@gmail.com>
2023-09-08 13:48:01 -0600

    Create initial duplicate finder using script with os, hashlib and collections

---
+++ superDeDuper.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+import hashlib
+import os
+
+# Function to calculate the hash of a file
+
+
+def hash_file(file_path):
+    hasher = hashlib.md5()
+    with open(file_path, 'rb') as file:
+        while True:
+            data = file.read(65536)  # Read in 64k chunks
+            if not data:
+                break
+            hasher.update(data)
+    return hasher.hexdigest()
+
+# Function to find and identify duplicate files
+
+
+def find_duplicates(start_directory):
+    file_hashes = defaultdict(list)
+
+    for root, dirs, files in os.walk(start_directory):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            file_hash = hash_file(file_path)
+            file_hashes[file_hash].append(file_path)
+
+    # Print duplicates
+    for hash_value, file_paths in file_hashes.items():
+        if len(file_paths) > 1:
+            print("Duplicate files (hash {}):".format(hash_value))
+            for path in file_paths:
+                print("  -", path)
+
+
+if __name__ == "__main__":
+    # start_directory = "/path/to/your/root/directory"
+    start_directory = "/Volumes/ImageFields/WildPasture/107_FUJI"
+
+    find_duplicates(start_directory)
